4bf133e8b8578ad77556d0233a3f893823df5795
Alexander Wallin <office@alexanderwallin.com>
2017-03-09 15:43:10 +0000

    Adds support for Swedish characters å, ä and ö

--- index.js
+++ index.js
@@ -9,5 +9,5 @@
 
 // Modified from: https://github.com/lepture/editor/blob/master/src/intro.js#L343
 module.exports = function () {
-  return /[a-zA-Z0-9_\u0392-\u03c9\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|\w+/g;
+  return /[a-zA-Z0-9_\u0392-\u03c9\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|[\u00E4\u00C4\u00E5\u00C5\u00F6\u00D6]+|\w+/g;
 };
